b5c2f006684ead3e4ec956bb522659d58afc938f
era009 <era009@uib.com>
2024-01-19 14:17:20 +0100

    new class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,11 +1,4 @@
-/*
- *RewardValue must have two constructors: one that accepts a cash value and one that accepts a value in miles.
-RewardValue must have a getCashValue() method, which returns the cash value of the RewardValue.
-RewardValue must have a getMilesValue() method, which returns how many miles the RewardValue is worth.
-RewardValue must convert from miles to cash at a rate of 0.0035.
- */
-
- public class RewardValue {
+public class RewardValue {
     private double cashValue;
     private int milesValue;
 
@@ -30,5 +23,4 @@ RewardValue must convert from miles to cash at a rate of 0.0035.
     public int getMilesValue() {
         return milesValue;
     }
-
 }
